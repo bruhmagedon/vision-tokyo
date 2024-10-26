c0d6192cdf51b90ed119eb21d58dd28dc1867bb7
bruhmagedon <schubert.walk@yandex.ru>
2024-10-26 23:47:41 +0300

    add prettier

--- src/app/layout/BaseLayout.tsx
+++ src/app/layout/BaseLayout.tsx
@@ -2,13 +2,13 @@ import { Outlet } from 'react-router-dom';
 import styles from './BaseLayout.module.scss';
 
 const BaseLayout = () => {
-    return (
-        <div className={styles.layout}>
-            <h1>jost</h1>
-            <span>BaseLayout</span>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div className={styles.layout}>
+      <h1>jost</h1>
+      <span>BaseLayout</span>
+      <Outlet />
+    </div>
+  );
 };
 
 export default BaseLayout;
